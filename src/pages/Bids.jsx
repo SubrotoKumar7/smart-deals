f6c85d844f468f8b1e680b28efa173beb6bbc8fd
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-11-07 04:58:32 +0600

    send token to server

--- src/pages/Bids.jsx
+++ src/pages/Bids.jsx
@@ -10,14 +10,18 @@ const Bids = () => {
 
     useEffect(()=> {
         if(user?.email){
-            fetch(`http://localhost:3000/bids?email=${user?.email}`)
+            fetch(`http://localhost:3000/bids?email=${user?.email}`, {
+                headers: {
+                    authorization: `Bearer ${user.accessToken}`
+                }
+            })
             .then(res=> res.json())
             .then(data=> {
                 console.log('after ', data);
                 setBids(data);
             })
         }
-    }, [user?.email]);
+    }, [user]);
 
     const handleRemoveBid = (_id) => {
         Swal.fire({
